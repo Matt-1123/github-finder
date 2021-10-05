e52cc0bb2212226af8b65c788a1dead7a3ac6d5d
Matt Russo <matthew.russo91@gmail.com>
2021-10-04 20:32:57 -0400

    add Navbar component
    add UserItem component with hard-coded state for testing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Fragment, Component } from "react";
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem.js";
 import "./App.css";
 
 class App extends Component {
   // Render is a lifecycle method that runs with the component is loaded
   render() {
-    const name = "John Doe";
-    const loading = false;
-    const showName = true;
-
     return (
-      <Fragment>
-        {loading ? (
-          <h4>Loading...</h4>
-        ) : (
-          <h1>Hello {showName && name.toUpperCase()}</h1>
-        )}
-      </Fragment>
+      <div className="App">
+        <Navbar />
+        <UserItem />
+      </div>
     );
   }
 }
