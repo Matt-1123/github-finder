3ddfd0a3e886e6b3a9ec6042d9490cfacc45c1da
Matt Russo <matthew.russo91@gmail.com>
2021-10-05 22:39:18 -0400

    add 'Users' component and generate list of users using UserItem component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem.js";
+import Users from "./components/users/Users.js";
 import "./App.css";
 
 class App extends Component {
@@ -9,7 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
