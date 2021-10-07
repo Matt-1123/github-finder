923bf58bea07b2936fadbec7d527831cb329fcf4
Matt Russo <matthew.russo91@gmail.com>
2021-10-06 20:03:59 -0400

    fetch data from github users api and add to app-level state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users.js";
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  // Lifecycle method: Runs when component mounts
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   // Render is a lifecycle method that runs with the component is loaded
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
