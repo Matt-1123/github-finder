c089f8652f9eb7acc69e765bd94520d8f8a7565c
Matt Russo <matthew.russo91@gmail.com>
2021-10-10 15:25:35 -0400

    add User route and getUser method

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 // Link helps maintain state when switching between routes as opposed to standard a tags which refresh the state
+// i.e. The link component intercepts requests to the server (browser) so that React can handle them. With react there's no need for the browser to retrieve and send index.html again
 import { Link } from "react-router-dom";
 
 const Navbar = ({ icon, title }) => {
