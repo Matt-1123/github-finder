bb193fde8e0407d0ba634e4c6e0868546e5c7b64
Matt Russo <matthew.russo91@gmail.com>
2021-10-17 13:57:00 -0400

    begin implementing context api

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import User from "./components/users/User.js";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
-import "./App.css";
 import axios from "axios";
+
+import GithubState from "./context/github/GithubState";
+
+import "./App.css";
 
 const App = () => {
   const [users, setUsers] = useState([]);
@@ -66,51 +69,55 @@ const App = () => {
   };
 
   return (
-    // Wrap Router around entire app so that all nested components have access to this Router component
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert} />
-          {/* Switch ensures only one route is shown at a time, i.e. prevents multiple pages from accidentally showing */}
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={(props) => (
-                <Fragment>
-                  <Search
-                    searchUsers={searchUsers}
-                    clearUsers={clearUsers}
-                    showClear={users.length > 0 ? true : false}
-                    setAlert={showAlert}
-                  />
-                  <Users loading={loading} users={users} />
-                </Fragment>
-              )}
-            />
-            <Route exact path="/about" component={About} />
-            {/* Note: :login represents the parameter at the end of the URL in the browser
+    // Wrap entire app in GithubState Provider
+    <GithubState>
+      {/* Wrap Router around entire app so that all nested components have access 
+      to this Router component*/}
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert} />
+            {/* Switch ensures only one route is shown at a time, i.e. prevents multiple pages from accidentally showing */}
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={searchUsers}
+                      clearUsers={clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={showAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              {/* Note: :login represents the parameter at the end of the URL in the browser
               e.g. /user/matt-1123 */}
-            {/* uses match.path for its path prop and then dynamically changes the UI. */}
-            <Route
-              exact
-              path="/user/:login"
-              render={(props) => (
-                <User
-                  {...props}
-                  getUser={getUser}
-                  getUserRepos={getUserRepos}
-                  user={user}
-                  repos={repos}
-                  loading={loading}
-                />
-              )}
-            />
-          </Switch>
+              {/* uses match.path for its path prop and then dynamically changes the UI. */}
+              <Route
+                exact
+                path="/user/:login"
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={getUser}
+                    getUserRepos={getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading}
+                  />
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </GithubState>
   );
 };
 
